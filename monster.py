e52322e05a90a168b16f586f080577ccb59b3134
ilyanelloyd-collab <ilyanelloyd@gmail.com>
2026-09-14 15:15:06 +0300

    Fix monster loot setup and speed range

--- monster.py
+++ monster.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 import animation
+
+
 class Monster(animation.AnimateSprite):
     def __init__(self, game, name, size, offset=0):
         super().__init__(name, size)
@@ -16,7 +18,7 @@ class Monster(animation.AnimateSprite):
 
     def set_speed(self, speed):
         self.default_speed = speed
-        self.velocity = random.randint(1, 3)
+        self.velocity = random.randint(1, speed)
 
     def set_loot_amount(self, amount):
         self.loot_amount = amount
@@ -24,33 +26,40 @@ class Monster(animation.AnimateSprite):
     def damage(self, amount):
         self.health -= amount
         if self.health <= 0:
-           self.rect.x = 1940 + random.randint(0, 300)
-           self.velocity = random.randint(1, self.default_speed)
-           self.health = self.max_health
-           self.game.add_score (self.loot_amount)
-           if self.game.comet_event.is_full_loaded():
-               self.game.all_monsters.remove(self)
-               self.game.comet_event.attempt_fall()
-
-
-
+            self.rect.x = 1940 + random.randint(0, 300)
+            self.velocity = random.randint(1, self.default_speed)
+            self.health = self.max_health
+            self.game.add_score(self.loot_amount)
+            if self.game.comet_event.is_full_loaded():
+                self.game.all_monsters.remove(self)
+                self.game.comet_event.attempt_fall()
 
     def update(self, surface):
-        pygame.draw.rect(surface, (0, 0 ,0) , [self.rect.x + 10, self.rect.y - 10, self.max_health, 5])
-        pygame.draw.rect(surface, (11, 210, 46), [self.rect.x + 10, self.rect.y - 10, self.health, 5])
+        pygame.draw.rect(
+            surface,
+            (0, 0, 0),
+            [self.rect.x + 10, self.rect.y - 10, self.max_health, 5],
+        )
+        pygame.draw.rect(
+            surface,
+            (11, 210, 46),
+            [self.rect.x + 10, self.rect.y - 10, self.health, 5],
+        )
         self.animate(loop=True)
 
     def forward(self):
         if not self.game.check_collision(self, self.game.all_players):
-         self.rect.x -= self.velocity
+            self.rect.x -= self.velocity
         else:
             self.game.player.damage(self.attack)
+
 
 class mummy(Monster):
     def __init__(self, game):
         super().__init__(game, "mummy", (130, 130))
         self.set_speed(3)
-        self.set_loot_amount = 20
+        self.set_loot_amount(20)
+
 
 class alien(Monster):
     def __init__(self, game):
@@ -59,5 +68,4 @@ class alien(Monster):
         self.max_health = 250
         self.attack = 0.08
         self.set_speed(1)
-        self.set_loot_amount = 80
-
+        self.set_loot_amount(80)
